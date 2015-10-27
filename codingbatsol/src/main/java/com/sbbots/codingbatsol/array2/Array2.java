742f9faa963e356d937026c187f6413d175b7716
sbbots <bansalsandeep1@gmail.com>
2015-10-26 23:14:52 -0700

    Added shiftLeft

--- codingbatsol/src/main/java/com/sbbots/codingbatsol/array2/Array2.java
+++ codingbatsol/src/main/java/com/sbbots/codingbatsol/array2/Array2.java
@@ -520,4 +520,24 @@ public class Array2 {
 		return arr;
 	}
 	
+	/*
+	 * Return an array that is "left shifted" by one -- so {6, 2, 5, 3} returns
+	 * {2, 5, 3, 6}. You may modify and return the given array, or return a new
+	 * array.
+	 * 
+	 * shiftLeft({6, 2, 5, 3}) → {2, 5, 3, 6} shiftLeft({1, 2}) → {2, 1}
+	 * shiftLeft({1}) → {1}
+	 */
+	public int[] shiftLeft(int[] nums) {
+
+		int[] arr = new int[nums.length];
+
+		for (int i = 1; i < arr.length; i++) {
+			arr[i - 1] = nums[i];
+		}
+		if (arr.length > 0) {
+			arr[nums.length - 1] = nums[0];
+		}
+		return arr;
+	}
 }
