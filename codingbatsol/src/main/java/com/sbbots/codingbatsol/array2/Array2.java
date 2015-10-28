dec04a9dd94085ac56c94e9d3faaa25b1cb63afd
sbbots <bansalsandeep1@gmail.com>
2015-10-27 20:54:41 -0700

    Added pre4,  post4

--- codingbatsol/src/main/java/com/sbbots/codingbatsol/array2/Array2.java
+++ codingbatsol/src/main/java/com/sbbots/codingbatsol/array2/Array2.java
@@ -1,5 +1,7 @@
 package com.sbbots.codingbatsol.array2;
 
+import java.util.Arrays;
+
 public class Array2 {
 	
 	public static void main(String[] args) {
@@ -571,4 +573,54 @@ public class Array2 {
 		}
 		return nums;
 	}
+	
+	/*
+	 * Given a non-empty array of ints, return a new array containing the
+	 * elements from the original array that come before the first 4 in the
+	 * original array. The original array will contain at least one 4. Note that
+	 * it is valid in java to create an array of length 0.
+	 * 
+	 * pre4({1, 2, 4, 1}) → {1, 2} 
+	 * pre4({3, 1, 4}) → {3, 1} 
+	 * pre4({1, 4, 4}) → {1}
+	 */
+	public int[] pre4(int[] nums) {
+		int i = 0;
+		for (i = 0; i < nums.length; i++) {
+			if (nums[i] == 4) {
+				break;
+			}
+		}
+		nums = Arrays.copyOf(nums, i);
+		return nums;
+	}
+	
+	
+	/*
+	 * Given a non-empty array of ints, return a new array containing the
+	 * elements from the original array that come after the last 4 in the
+	 * original array. The original array will contain at least one 4. Note that
+	 * it is valid in java to create an array of length 0.
+	 * 
+	 * post4({2, 4, 1, 2}) → {1, 2} 
+	 * post4({4, 1, 4, 2}) → {2} 
+	 * post4({4, 4, 1, 2, 3}) → {1, 2, 3}
+	 */
+	public int[] post4(int[] nums) {
+
+		int i = 0;
+		int len = nums.length;
+		for (i = len - 1; i > -1; i--) {
+			if (nums[i] == 4) {
+				break;
+			}
+		}
+		int[] arr = new int[len - i - 1];
+		for (int j = 0; j < arr.length && (i + j + 1) < nums.length; j++) {
+
+			arr[j] = nums[i + j + 1];
+		}
+		return arr;
+	}
+
 }
