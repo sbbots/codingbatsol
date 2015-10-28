07adae39f555c63407c30e5d44ff5ec05e53e207
sbbots <bansalsandeep1@gmail.com>
2015-10-27 21:08:04 -0700

    Added notAlone

--- codingbatsol/src/main/java/com/sbbots/codingbatsol/array2/Array2.java
+++ codingbatsol/src/main/java/com/sbbots/codingbatsol/array2/Array2.java
@@ -630,4 +630,29 @@ public class Array2 {
 		return arr;
 	}
 
+	/*
+	 * We'll say that an element in an array is "alone" if there are values
+	 * before and after it, and those values are different from it. Return a
+	 * version of the given array where every instance of the given value which
+	 * is alone is replaced by whichever value to its left or right is larger.
+	 * 
+	 * notAlone({1, 2, 3}, 2) → {1, 3, 3} 
+	 * notAlone({1, 2, 3, 2, 5, 2}, 2) → {1, 3, 3, 5, 5, 2} 
+	 * notAlone({3, 4}, 3) → {3, 4}
+	 */
+	public int[] notAlone(int[] nums, int val) {
+
+		int i = 0;
+		int len = nums.length;
+		for (i = 0; i < len; i++) {
+
+			if ((i > 0 && i < len - 1)
+					&& (nums[i] == val && nums[i - 1] != val && nums[i + 1] != val)) {
+				nums[i] = nums[i - 1] > nums[i + 1] ? nums[i - 1] : nums[i + 1];
+			}
+		}
+
+		return nums;
+	}
+
 }
