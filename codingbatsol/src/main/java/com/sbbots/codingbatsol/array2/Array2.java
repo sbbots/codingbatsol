2e5d60577f8b63db77a3f7c0ce51096e77fd42ac
sbbots <bansalsandeep1@gmail.com>
2015-10-27 21:13:41 -0700

    Added zeroFront

--- codingbatsol/src/main/java/com/sbbots/codingbatsol/array2/Array2.java
+++ codingbatsol/src/main/java/com/sbbots/codingbatsol/array2/Array2.java
@@ -651,7 +651,34 @@ public class Array2 {
 				nums[i] = nums[i - 1] > nums[i + 1] ? nums[i - 1] : nums[i + 1];
 			}
 		}
+		return nums;
+	}
 
+	/*
+	 * Return an array that contains the exact same numbers as the given array,
+	 * but rearranged so that all the zeros are grouped at the start of the
+	 * array. The order of the non-zero numbers does not matter. So {1, 0, 0, 1}
+	 * becomes {0 ,0, 1, 1}. You may modify and return the given array or make a
+	 * new array.
+	 * 
+	 * zeroFront({1, 0, 0, 1}) → {0, 0, 1, 1} 
+	 * zeroFront({0, 1, 1, 0, 1}) → {0, 0, 1, 1, 1} 
+	 * zeroFront({1, 0}) → {0, 1}
+	 */
+	public int[] zeroFront(int[] nums) {
+
+		int len = nums.length;
+		int swapIndex = 0;
+		int temp;
+		for (int i = 0; i < len; i++) {
+
+			if (nums[i] == 0) {
+				temp = nums[swapIndex];
+				nums[swapIndex] = nums[i];
+				nums[i] = temp;
+				swapIndex++;
+			}
+		}
 		return nums;
 	}
 
